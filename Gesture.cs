bad92eb169f6e634ae911d82168055281207bc5b
Dima <fufajka01@gmail.com>
2016-09-04 10:30:31 +0300

    Небольшие правки

--- Gesture.cs
+++ Gesture.cs
@@ -8,9 +8,13 @@ public class Gesture
 	List<DatasetItem>[] _fingers;
 	DateTime _timeStamp;
 
+	public Rectangle[] FingerRectangles;
+
+
 	public Gesture ()
 	{
 		_timeStamp = DateTime.Now;
+		FingerRectangles = new Rectangle[5];
 	}
 
 	public int CheckGesture(List<DatasetItem>[] fingers)
@@ -22,23 +26,19 @@ public class Gesture
 		_timeStamp = DateTime.Now;
 		_fingers = fingers;
 
-
-		Rectangle[] FingerRectangles = new Rectangle[5];
-
 		for (int i = 0; i < _fingers.Length; i++)
 		{
 			var item = _fingers[i];
 			Rectangle ThumbRect = new Rectangle((int) item.Min(a => a.X),
-											(int) item.Min(a => a.Y),
-											(int) item.Max(a => a.X) - (int) item.Min(a => a.X),
-											(int) item.Max(a => a.Y) - (int) item.Min(a => a.Y));
+				(int) item.Min(a => a.Y),
+				(int) item.Max(a => a.X) - (int) item.Min(a => a.X),
+				(int) item.Max(a => a.Y) - (int) item.Min(a => a.Y));
 			FingerRectangles[i] = ThumbRect;
 		}
 
 		for (int i = 1; i < _fingers.Length; i++) {
-            int Distance = GetDistance(Center(FingerRectangles[(int)FingersName.THUMB]), Center(FingerRectangles[i]));
-
-            if (Distance <= 50)
+			if (GetDistance(Center(FingerRectangles[(int)FingersName.THUMB]), 
+				Center(FingerRectangles[i])) <= 50)
 				return i;
 		}
 
@@ -47,16 +47,10 @@ public class Gesture
 
 	int GetDistance(Point x, Point y)
 	{
-        int x1, x2, y1, y2;
-        x1 = x.X;
-        x2 = y.X;
-        y1 = x.Y;
-        y2 = y.Y;
-
-		return (int) Math.Sqrt(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+		return (int) Math.Sqrt((x.X - y.X) * (x.X - y.X) + (x.Y - y.Y) * (x.Y - y.Y));
 	}
 
-	Point Center(Rectangle rect)
+	public Point Center(Rectangle rect)
 	{
 		return new Point(rect.Left + rect.Width/2,
 			rect.Top + rect.Height / 2);
